f6a85f5600aee18b43bbba7411ae25eef78f2284
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-10-14 07:06:04 +0200

    :sparkles: Implement PO converters

---
+++ src/ResxConverter/Resx/Resx2Po.cs
@@ -0,0 +1,41 @@
+﻿namespace PleOps.Il28n.ResxConverter.Resx;
+
+using Yarhl.FileFormat;
+using Yarhl.Media.Text;
+
+public class Resx2Po : IConverter<LocalizedResxCatalog, Po>
+{
+    private readonly string projectId;
+    private readonly string reporter;
+    private readonly LocalizedResxCatalog sourceCatalog;
+
+    public Resx2Po(string projectId, string reporter, LocalizedResxCatalog sourceCatalog)
+    {
+        ArgumentNullException.ThrowIfNull(projectId);
+        ArgumentNullException.ThrowIfNull(reporter);
+        ArgumentNullException.ThrowIfNull(sourceCatalog);
+
+        this.projectId = projectId;
+        this.reporter = reporter;
+        this.sourceCatalog = sourceCatalog;
+    }
+
+    public Po Convert(LocalizedResxCatalog targetCatalog)
+    {
+        var header = new PoHeader(projectId, reporter, targetCatalog.Language);
+        var po = new Po(header);
+
+        foreach (LocalizedResxMessage sourceEntry in sourceCatalog.Messages) {
+            LocalizedResxMessage? targetEntry = targetCatalog.Messages
+                .FirstOrDefault(m => m.Id == sourceEntry.Id);
+
+            var poEntry = new PoEntry(sourceEntry.Value!) {
+                Context = sourceEntry.Id,
+                Translated = targetEntry?.Value ?? string.Empty,
+            };
+            po.Add(poEntry);
+        }
+
+        return po;
+    }
+}
---
+++ src/ResxConverter/Resx/Resx2Pot.cs
@@ -0,0 +1,34 @@
+﻿namespace PleOps.Il28n.ResxConverter.Resx;
+
+using Yarhl.FileFormat;
+using Yarhl.Media.Text;
+
+public class Resx2Pot : IConverter<LocalizedResxCatalog, Po>
+{
+    private readonly string projectId;
+    private readonly string reporter;
+
+    public Resx2Pot(string projectId, string reporter)
+    {
+        ArgumentNullException.ThrowIfNull(projectId);
+        ArgumentNullException.ThrowIfNull(reporter);
+
+        this.projectId = projectId;
+        this.reporter = reporter;
+    }
+
+     public Po Convert(LocalizedResxCatalog source)
+    {
+        var header = new PoHeader(projectId, reporter, source.Language);
+        var po = new Po(header);
+
+        foreach (LocalizedResxMessage sourceEntry in source.Messages) {
+            var poEntry = new PoEntry(sourceEntry.Value!) {
+                Context = sourceEntry.Id,
+            };
+            po.Add(poEntry);
+        }
+
+        return po;
+    }
+}
